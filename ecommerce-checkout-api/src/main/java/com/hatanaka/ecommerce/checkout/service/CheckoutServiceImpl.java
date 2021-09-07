9d0c9b63314850b6004090b562390d1e0bca669b
nei <nei.thomass@gmail.com>
2021-09-07 10:39:47 -0300

    renomeia paths

--- ecommerce-checkout-api/src/main/java/com/hatanaka/ecommerce/checkout/service/CheckoutServiceImpl.java
+++ ecommerce-checkout-api/src/main/java/com/hatanaka/ecommerce/checkout/service/CheckoutServiceImpl.java
@@ -3,6 +3,8 @@ package com.hatanaka.ecommerce.checkout.service;
 import com.hatanaka.ecommerce.checkout.entity.CheckoutEntity;
 import com.hatanaka.ecommerce.checkout.repository.CheckoutRepository;
 import com.hatanaka.ecommerce.checkout.resource.checkout.CheckoutRequest;
+import com.hatanaka.ecommerce.checkout.streaming.CheckoutCreatedSource;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
@@ -14,14 +16,27 @@ import java.util.UUID;
 public class CheckoutServiceImpl implements CheckoutService{
 
     private final CheckoutRepository checkoutRepository;
+    private final CheckoutCreatedSource checkoutCreatedSource;
 
     @Override
     public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
+
         final CheckoutEntity checkoutEntity = CheckoutEntity
                 .builder()
                 .code(UUID.randomUUID().toString())
                 .status(CheckoutEntity.Status.CREATED)
                 .build();
-        return Optional.of(checkoutRepository.save(checkoutEntity));
+
+        final CheckoutEntity entity = checkoutRepository
+                .save(checkoutEntity);
+
+        final CheckoutCreatedEvent checkoutCreatedEvent = CheckoutCreatedEvent
+                .newBuilder()
+                .setCheckoutCode(entity.getCode())
+                .setStatus(entity.getStatus().name())
+                .build();
+
+        checkoutCreatedSource.output().send(MessageBuilder.withPayload(checkoutCreatedEvent).build());
+        return Optional.of(entity);
     }
 }
